e1dee7eaee3d5ae837e4299a1ee48a23285c487a
ultreson <ultreson@outlook.com>
2021-03-08 16:31:53 -0500

    fully featured help and functions encapsulation added

--- log-current-installed-packages.sh
+++ log-current-installed-packages.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
-# this script requires a date as a parameter
+# Author : https://github.com/ultreson
+# Creation Date : 2021-03-08
+# this script requires a date as a parameter as $1
+
+
+display_help() {
+  echo "Usage:" >&2
+  echo "  log-current-installed-packages.sh [fileName]"
+  exit 1
+}
+
+savePackages() {
+  save_path=$HOME'/software installers/'
+  log_extension='.list'
+  final_save_path=$save_path
+  final_save_path+=$1
+  final_save_path+=$log_extension
+  #apt list --manual-installed | grep -F \[installed\] 
+  apt-mark showmanual > "${final_save_path}"
+  echo "saved packages to \"${final_save_path}\""
+}
+
 if [ $# -eq 0 ]
   then
-    echo "No argument supplied"
-    echo "please supply date under the format yyyy-mm-dd"
+    echo "Error: No argument supplied"
+    echo "  please supply date under the format yyyy-mm-dd"
+    display_help
 fi
-#apt list --manual-installed | grep -F \[installed\] 
-apt-mark showmanual > ~/software\ installers/$1.list
+
+savePackages $1
+
